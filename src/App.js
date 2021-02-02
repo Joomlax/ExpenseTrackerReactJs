8cb7f1480a9dd57159d2c4bae80a8766eeda60bf
Dogukan <dogukangulyasar@gmail.com>
2021-02-02 14:48:08 +0300

    Added transaction list and balance components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
